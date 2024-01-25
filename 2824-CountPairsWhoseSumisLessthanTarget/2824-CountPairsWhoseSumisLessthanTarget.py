07ff9c2fb4d4195d3e9629a088f067b1a5c52dda
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-01-25 15:34:07 -0800

    Update file 1/25/2024, 3:34:05 PM

--- 2824-CountPairsWhoseSumisLessthanTarget/2824-CountPairsWhoseSumisLessthanTarget.py
+++ 2824-CountPairsWhoseSumisLessthanTarget/2824-CountPairsWhoseSumisLessthanTarget.py
@@ -1,17 +1,16 @@
-class Solution:
-    def countPairs(self, nums: List[int], target: int) -> int:
-        
-        nums.sort()
 
         l, r, res = 0, len(nums) - 1, 0
 
-        while l <= r:
+        while l < r:
             if nums[l] + nums[r] < target:
         #[-1,1,1,2,3]
-                res += l + r
+                res += r - l
                 l += 1
             else:
                 r -= 1
         return res
-
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        
+        nums.sort()
 [-1,1,2,3,1]
