2a4688d3901f5ebb4a9fa243e010b5f99f0e1542
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-01-25 15:13:48 -0800

    Create file

---
+++ 2798-NumberofEmployeesWhoMettheTarget/2798-NumberofEmployeesWhoMettheTarget.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        res = 0
+
+        for i in hours:
+            if i >= target: res += 1
+
+        return res
+[0,1,2,3,4]
