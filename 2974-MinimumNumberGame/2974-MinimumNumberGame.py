8a77ab48985b25a628429ebaf07c6c5df358dbe9
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-01-25 16:16:24 -0800

    Update file 1/25/2024, 4:16:22 PM

--- 2974-MinimumNumberGame/2974-MinimumNumberGame.py
+++ 2974-MinimumNumberGame/2974-MinimumNumberGame.py
@@ -1,17 +1,11 @@
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        res = []
-
-        while nums:
-            aliceMin = min(nums)
-            nums.remove(aliceMin)
-            bobMin = min(nums)
-            nums.remove(bobMin)
-
-
-            res.append(bobMin)
-            res.append(aliceMin)
-        
-        return res
+        nums.sort()
+        res = [0] * len(nums)
+        for i in range(1, len(nums), 2):
             
+            nums[i], nums[i - 1] = nums[i - 1], nums[i]
+
+        return nums
+
 [5,4,2,3]
