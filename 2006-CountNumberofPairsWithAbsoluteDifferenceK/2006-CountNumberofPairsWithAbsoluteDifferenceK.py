38af9638275cb909760c750bb66c163d2cca65a8
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-01-26 19:04:59 -0800

    Update file 1/26/2024, 7:04:59 PM

--- 2006-CountNumberofPairsWithAbsoluteDifferenceK/2006-CountNumberofPairsWithAbsoluteDifferenceK.py
+++ 2006-CountNumberofPairsWithAbsoluteDifferenceK/2006-CountNumberofPairsWithAbsoluteDifferenceK.py
@@ -1,11 +1,9 @@
-class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
         temp = defaultdict(int)
         res = 0
         for i in nums:
-            print(temp)
             res += temp[i-k] + temp[i+k]
-            print(temp[i-k], temp[i+k])
             temp[i] += 1
         return res
+class Solution:
 [1,2,2,1]
