7079c874853a40a42d8bf339725fec1b24f0bca0
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-01-30 17:08:15 -0800

    Create file

---
+++ 788-RotatedDigits/788-RotatedDigits.py
@@ -0,0 +1,12 @@
+            while j != 0:
+                d = j % 10
+            j = i
+            
+        for i in range(1,n + 1):
+
+        check = False
+        res = 0
+        temp = [3,4,7]
+    def rotatedDigits(self, n: int) -> int:
+class Solution:
+10
