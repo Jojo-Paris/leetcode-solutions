ebf8975f966bb1ce6c11295bfb786464cce8516b
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-01-30 18:26:04 -0800

    Update file 1/30/2024, 6:26:04 PM

--- Question1/Question1.py
+++ Question1/Question1.py
@@ -1,12 +1,20 @@
-        else:
-            x = abs(x)
-            x = str(x)
-            x = x[::-1]
-            x = int(x) * -1
-            x = x[::-1]
-        if x < 0:
-            x = abs(x)
-            x = str(x)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def reverse(self, x: int) -> int:
-123
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+
+        def sumOfLeft(root, isLeft):
+            if root is None: return 0
+
+            if root.left is None and root.right is None and isLeft:
+                return root.val
+
+            return sumOfLeft(root.left, True) + sumOfLeft(root.right, False)
+        
+        return sumOfLeft(root, False)
+        
+[3,9,20,null,null,15,7]
