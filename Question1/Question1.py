fa97d2634ab7e45908ca696c32779d28637d6609
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-01-30 16:26:54 -0800

    Create file

---
+++ Question1/Question1.py
@@ -0,0 +1,12 @@
+        else:
+            x = abs(x)
+            x = str(x)
+            x = x[::-1]
+            x = int(x) * -1
+            x = x[::-1]
+        if x < 0:
+            x = abs(x)
+            x = str(x)
+class Solution:
+    def reverse(self, x: int) -> int:
+123
