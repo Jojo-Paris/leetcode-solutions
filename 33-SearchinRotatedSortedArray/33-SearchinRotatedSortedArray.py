c71b7c0ef035e401442bece2ed30acea6e8b85e1
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-06 19:25:29 -0800

    Update file 2/6/2024, 7:25:29 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
@@ -1,12 +1,10 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
 
         while l <= r:
 
             mid = (l + r) // 2
             if nums[mid] == target: return mid
-            if nums[mid] > nums[l]:
+            if nums[l] <= nums[mid]:
         
         return -1
                 if target > nums[mid] or target < nums[l]: l = mid + 1
@@ -16,4 +14,6 @@ class Solution:
                 if target < nums[mid] or target > nums[r]: r = mid - 1
                 else: l = mid + 1
 
+    def search(self, nums: List[int], target: int) -> int:
+class Solution:
 [4,5,6,7,0,1,2]
