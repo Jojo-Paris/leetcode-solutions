e7ca379bf540fb4a6e78f3cb5c6125bf003a4cb8
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-08 14:15:05 -0800

    Update file 2/8/2024, 2:15:05 PM

--- 143-ReorderList/143-ReorderList.py
+++ 143-ReorderList/143-ReorderList.py
@@ -7,22 +7,22 @@
 
         first = head
 
-        while curr:
+        while second:
             
         second = prev
             temp1, temp2 = first.next, second.next
 
+        slow.next = None
+        prev = None
+            fast = fast.next.next
+        
+        curr = slow.next
+
+        while fast and fast.next:
+            slow = slow.next
             first.next = second
             second.next = temp1
             first = temp1
-        slow.next = None
-        prev = None
             second = temp2
 
-
-
-        
-
-
-        
 [1,2,3,4]
