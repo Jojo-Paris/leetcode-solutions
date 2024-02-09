faece76fd314a6fa05cefe05a3227f2a728dcfb9
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-09 14:43:04 -0800

    Update file 2/9/2024, 2:43:04 PM

--- 226-InvertBinaryTree/226-InvertBinaryTree.py
+++ 226-InvertBinaryTree/226-InvertBinaryTree.py
@@ -1,21 +1,21 @@
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+      
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
-#         self.right = right
-class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
         if not root:
+            
             return None
         
-        #swap the children
-        tmp = root.left
-        root.left = root.right
-        root.right = tmp
+        return root
+        #swap
 
-        #recurisvely call on both left and right
-        self.invertTree(root.left) 
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
         self.invertTree(root.right)
+
 [4,2,7,1,3,6,9]
