8528291a50ea983a7adccb308738653e380eb40f
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-12 16:18:27 -0800

    Update file 2/12/2024, 4:18:27 PM

--- 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.py
+++ 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.py
@@ -8,14 +8,14 @@
             return True
         
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-        if root and subroot:
+        if root and subRoot:
         if not subRoot: return True 
-            return True
-        if root and subroot and root.val == subRoot.val:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
 class Solution:
 #         self.right = right
 #         self.left = left
 #         self.val = val
 #     def __init__(self, val=0, left=None, right=None):
+# class TreeNode:
+# Definition for a binary tree node.
 [3,4,5,1,2]
