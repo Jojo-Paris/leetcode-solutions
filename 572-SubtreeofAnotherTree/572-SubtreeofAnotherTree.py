7008758160a2eb992d0a2977f8771d25d746c9af
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-12 16:18:00 -0800

    Update file 2/12/2024, 4:18:00 PM

--- 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.py
+++ 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.py
@@ -1,17 +1,21 @@
-        
-        if not subRoot: return True
+
+
+    def isSame(self, root, subRoot):
+
         if not root: return False
 
-        if self.treeMatch(root, subRoot):
+        if self.isSame(root, subRoot):
             return True
         
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) 
-
-    def treeMatch(self, root, subRoot):
-        if not root and not subRoot:
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        if root and subroot:
+        if not subRoot: return True 
             return True
-        if root and subRoot and root.val == subRoot.val:
-            return self.treeMatch(root.left, subRoot.left) and self.treeMatch(root.right, subRoot.right)
-        else:
-            return False
+        if root and subroot and root.val == subRoot.val:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+class Solution:
+#         self.right = right
+#         self.left = left
+#         self.val = val
+#     def __init__(self, val=0, left=None, right=None):
 [3,4,5,1,2]
