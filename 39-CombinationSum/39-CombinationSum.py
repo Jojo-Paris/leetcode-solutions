d6199fa28d0f87b8102f80c6bfb741d48c93da1d
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-15 15:39:38 -0800

    Update file 2/15/2024, 3:39:36 PM

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,18 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
+        res = []
 
         def backTrack(idx, arr, total): 
-
             if total == target:
-        res = []
                 res.append(arr.copy())
                 return 
             if total > target:
                 return
 
             for i in range(idx, len(candidates)):
-        backTrack(0, [], 0)
                 backTrack(i, arr + [candidates[i]], total + candidates[i])
+
+        backTrack(0, [], 0)
         return res
-[
+[2,3,6,7]
