33ebfefc1f43c177b5fd9de8df27cea85586c669
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-15 21:04:14 -0800

    Update file 2/15/2024, 9:04:11 PM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,11 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1,1
+        
+        if n == 1: return 1
 
-        for i in range(n - 1):
-            temp = one
-            one += two
-            two = temp
-            
-        return one
+        dp = [0 for i in range(n + 1)]
+
+        dp[0], dp[1] = 1, 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+            print(dp)
+        return dp[n]
+        
+
 2
