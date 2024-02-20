6060654d503611aa6afad684d87a25ff72be0dbd
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-19 17:10:55 -0800

    Create file

---
+++ 535-EncodeandDecodeTinyURL/535-EncodeandDecodeTinyURL.py
@@ -0,0 +1,18 @@
+class Codec:
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+        return longUrl
+        return shortUrl
+# codec.decode(codec.encode(url))
+"
