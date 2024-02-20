b7db6b6f3b466699bd463d9d31e33e7eac4c9f1f
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-19 20:04:20 -0800

    Update file 2/19/2024, 8:04:20 PM

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.py
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.py
@@ -13,7 +13,8 @@ class RecentCounter:
 
         while self.requests[0] < t - 3000: 
             self.requests.popleft()
-        
+            print(self.requests)
         return len(self.requests)
+            
 # param_1 = obj.ping(t)
 [
