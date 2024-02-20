d547097ab0060cfaa46c4b0b2c4a227301e5a977
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-19 19:15:48 -0800

    Create file

---
+++ 1261-FindElementsinaContaminatedBinaryTree/1261-FindElementsinaContaminatedBinaryTree.py
@@ -0,0 +1,27 @@
+
+    def __init__(self, root: Optional[TreeNode]):
+        
+        def recover(tempRoot, value):
+        self.A = A = set()
+
+            if not tempRoot: return None
+            
+        recover(root, 0)            
+#         self.right = right
+class FindElements:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+# Definition for a binary tree node.
+# class TreeNode:
+
+
+    def find(self, target: int) -> bool:
+        return target in self.A
+
+
+            A.add(value)
+            recover(tempRoot.left, 2*value + 1)
+            recover(tempRoot.right, 2*value + 2)
+
+[
