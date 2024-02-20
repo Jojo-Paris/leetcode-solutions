0078a62810058f8b70ed79ebe3b1d31ab600fd9d
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-19 19:31:21 -0800

    Create file

---
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.py
@@ -0,0 +1,19 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = []
+
+    def ping(self, t: int) -> int:
+        incRange = t - 3000
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+        self.requests.append(t)
+        for i in self.requests:
+                temp += 1
+            if incRange <= i <= t: 
+        temp = 0
+        return temp
+# param_1 = obj.ping(t)
+[
