0594df260f809e2ace26b0fb9bac9f5dc9312994
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-02-19 16:29:28 -0800

    Update file 2/19/2024, 4:29:28 PM

--- 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.py
+++ 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.py
@@ -1,6 +1,4 @@
 
-    def get(self, key: str, timestamp: int) -> str:
-
         while l <= r:
             mid = (l + r) // 2
             val = self.arr[key][mid][1]
@@ -10,19 +8,21 @@
             else:
                 r = mid - 1
 
-            
-            
-        values = self.arr.get(key, [])
-        l, r = 0, len(values) - 1
-        
-        return temp
-
-        self.arr[key].append([value, timestamp])
-        
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.arr:
-            self.arr[key] = []
                 temp = self.arr[key][mid][0]
 
             if val == timestamp: return self.arr[key][mid][0]
+    def get(self, key: str, timestamp: int) -> str:
+        values = self.arr.get(key, [])
+        l, r = 0, len(values) - 1
+        self.arr[key].append([value, timestamp])
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.arr:
+            self.arr[key] = []
+    def __init__(self):
+        self.arr = {}
+
+class TimeMap:
+
 [
