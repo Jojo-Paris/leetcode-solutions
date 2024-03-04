e6d8b8e9c811fcb4df9589217da279391c63e9bb
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-03-03 19:29:39 -0800

    Update file 3/3/2024, 7:29:38 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -4,11 +4,10 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
         dp[0] = 0
-        dp[1] = 1
 
         for a in range(1, amount + 1):
             for coin in coins:
-                a - c >= 0: dp[a] = min(dp[a], 1 + dp[a - c])
+                if a - coin >= 0: dp[a] = min(dp[a], 1 + dp[a - coin])
         
         return dp[amount] if dp[amount] != amount + 1 else -1
 [
