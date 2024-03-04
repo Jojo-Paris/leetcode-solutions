c37b29d88d51d135215be31d8c6b121cf9fd0184
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-03-03 19:01:19 -0800

    Create file

---
+++ 322-CoinChange/322-CoinChange.py
@@ -0,0 +1,24 @@
+        def dfs(curAmount, i, memo):
+            if curAmount < 0: return float('inf')
+
+            if curAmount == 0: 
+                res[0] = min(i, res[0])
+                return 0
+
+            for j in coins: 
+                minCoins = min(minCoins, 1 + dfs(curAmount - j, i + 1, memo))
+
+
+
+            if memo[curAmount] != -1:
+                return memo[curAmount]
+
+            minCoins = float('inf')
+            memo[curAmount] = minCoins
+
+            return minCoins
+        memo = [-1] * (amount + 1)
+        result = dfs(amount, 0, memo)
+        return result if result != float('inf') else -1
+
+[
