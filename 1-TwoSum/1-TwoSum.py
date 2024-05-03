0e405a2ce1b2fe6e4760faa71d5523436c9fd70c
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2024-05-03 11:44:49 -0700

    Update file 5/3/2024, 11:44:49 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,21 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        #loop through once to end of array
-        #save all numbers i find in a hashmap with that index
-        #if i subtract the target the a number and it is not in that hashmap we have not found it yet
-        #thus we need to add it inside, if it is found we return both the indices
-
-        n = len(nums)
-        temp = {}
-
-        for idx, val in enumerate(nums):
-            remainder = target - val
-
-            if remainder in temp:
-                return [temp[remainder], idx]
-            else:
-                temp[val] = idx
         
-        return None
-[2,7,11,15]
+        for idx, val in enumerate(nums):
+            output = target - val
+        Hash = {}
+
+
+            if output in Hash: return [Hash[output], idx]
+            else: Hash[val] = idx
+        
+        return -1
+
+[
